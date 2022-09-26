b357af459003f6d467388f451ab179a2a68f0b00
Tyler Caudill <103776658+TeeCaud@users.noreply.github.com>
2022-09-26 10:32:15 -0500

    feat bookservice test

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.descrbie BookService do
   it 'retrieves books from Openlibrary' do
+    id = 0201558025
     reponse = BookService.get_book(id)
-    expect()
+    expect(response).to be(successful)
+    expect(response).to be_a Hash
+    expect(response).to be_have(:ISBN)
+    expect(response[:ISBN]).to be_have(:title)
+    expect(response[:ISBN]).to be_have(:publishers)
   end
 end
