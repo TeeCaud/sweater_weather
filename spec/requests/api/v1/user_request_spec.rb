fb68b92ef40754dd1d6851047616a188a9ff85a1
Tyler Caudill <103776658+TeeCaud@users.noreply.github.com>
2022-09-27 11:52:13 -0500

    tests sad path for user creation

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'User API endpoint', :vcr do
   it 'can create a user' do
-    headers = { 'CONTENT_TYPE' => 'application/json' }
     body = {
             "email": "whatever@example.com",
             "password": "password",
@@ -12,10 +11,30 @@ RSpec.describe 'User API endpoint', :vcr do
     post '/api/v1/sign-up', params: body
     expect(response).to be_successful
 
-    parsed_body = JSON.parse(response.body, symbolize_names: true)
+    json = JSON.parse(response.body, symbolize_names: true)
+    user = json[:data]
+    expect(user).to have_key(:id)
+    expect(user[:id]).to be_a String
+    expect(user).to have_key(:type)
+    expect(user[:type]).to be_a String
+    attributes = user[:attributes]
+    expect(attributes).to have_key(:email)
+    expect(attributes).to have_key(:api_key)
+    expect(attributes[:email]).to be_a String
+    expect(attributes[:api_key]).to be_a String
+    expect(attributes).to_not have_key(:password)
+    expect(attributes).to_not have_key(:password_confirmation)
+  end
 
-    expect()
-
-
+  it 'has sad path for creating user' do
+    body = {
+            "email": "whatever@example.com",
+            "password": "password",
+            "password_confirmation": "not matching"
+            }
+    post '/api/v1/sign-up', params: body
+    expect(response).to_not be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:password_confirmation]).to eq(["doesn't match Password"])
   end
 end
