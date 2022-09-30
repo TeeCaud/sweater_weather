fb690fae67ce388a62c119798295de4383b980ed
Tyler Caudill <103776658+TeeCaud@users.noreply.github.com>
2022-09-30 17:01:30 -0500

    asd

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RoadTripsController < ApplicationController
 
   def create
+    binding.pry
     user = User.find_by(api_key: params[:api_key])
     if user.present?
       travel_time = GeocoderFacade.get_directions(params[:start_city], params[:end_city])
